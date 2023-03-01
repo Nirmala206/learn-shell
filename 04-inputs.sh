64ead83d5844074096e42d69f72f7c8fbccbcddc
Nirmala <nirmalapolisetty206@gmail.com>
2023-03-02 01:55:01 +0530

    Added simple variable program

---
+++ 04-inputs.sh
@@ -0,0 +1,11 @@
+#0-n , $*/$@, $#
+
+echo First arguement $1 #First arguement
+echo Second Arguement $2 #second arguement
+
+echo Script Name$0 # script Name
+
+echo All Arguments = $*
+eco All Arguments = $@  #  All Arguments
+
+echo Number of Arguments = $#  # Number of Arguments
