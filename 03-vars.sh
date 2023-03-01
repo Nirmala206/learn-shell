e0d32485201b4624ed2e592c1d38ff65ee8059ba
Nirmala <nirmalapolisetty206@gmail.com>
2023-03-02 01:40:39 +0530

    Added simple variable program

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,9 +1,12 @@
 DATE=28-02-2023
 echo "Hello, Good Morning, today date is ${DATE}"
 
+#Command substitution
 DATE=$(date)
 echo "Hello, Good Morning, today date is ${DATE}"
 
 #Arithmetic substitution
 ADD=$((2+3))
 echo "Addition of 2 & 3 is ${ADD}"
+
+ x=10
