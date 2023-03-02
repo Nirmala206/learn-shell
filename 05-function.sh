481d7c98f9b39cec47f78eef851b1ca08a75a7fa
Nirmala <nirmalapolisetty206@gmail.com>
2023-03-03 01:09:52 +0530

    Added function program

--- 05-function.sh
+++ 05-function.sh
@@ -2,7 +2,11 @@
 
 xyz(){
   echo hello from function
+  echo first Argument - $1
+  echo first Argument - $2
+  echo all Arguments - $*
+  echo No of Arguments - $#
 }
 
 ## Call a function
-xyz
+xyz 123 456
