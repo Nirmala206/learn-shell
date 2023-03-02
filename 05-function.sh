2f48599bdcae4db6886628530f94d57ea83628b4
Nirmala <nirmalapolisetty206@gmail.com>
2023-03-03 01:19:24 +0530

    variables with functions

--- 05-function.sh
+++ 05-function.sh
@@ -6,7 +6,12 @@ xyz(){
   echo first Argument - $2
   echo all Arguments - $*
   echo No of Arguments - $#
+  echo value of a = $a
+  b=200
 }
 
+## Main program
 ## Call a function
+a= 120
 xyz 123 456
+echo value of b - $b
